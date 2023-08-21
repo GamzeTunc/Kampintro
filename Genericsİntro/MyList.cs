3b05db60dac995be051b153cb684612709f7576e
GamzeTunc <tuncgamze35@gmail.com>
2023-08-21 23:59:17 +0300

    Generic Classlar Son hali

--- Genericsİntro/MyList.cs
+++ Genericsİntro/MyList.cs
@@ -12,14 +12,21 @@ namespace Genericsİntro
 
     {
         T[] items;//classı new lediğimde oluşturmam gerek
+        //string[] Meyveler = new string[] { "vxc","zxcz" }; bunu tanımlamadan string[] Meyveler; gibi normal dizi oluşturdu 
 
         public MyList() //constractor method. Classı bir yerde kullanırsam new lerser otomatik direk çalışır.
         {
-            
+            items = new T[0]; 
         }
         public void Add(T item)//item eleman demek 
         {
-
+            T[] tempArray = items; //yeni dizi oluşturduk adı geçiciDizi onun içine attık mevcut diziyi eski hali de silinmez bu sayede
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
         }
     }
 }
